a85627806a861ae5e29e6a1a810cfd4ad8ff57fc
Deb <deborah.tsao@gmail.com>
2018-10-30 12:45:12 -0700

    updated with list creation in HTML

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,7 +1,10 @@
 "use strict";
 
 var gamelist = [];
-var colors = ["yellow", "red", "blue", "green", "black", "orange", "pink", "purple"];
+
+//need to make a key value object for each color
+//to choose the actual shade that I want...or maybe I can do that in scss...
+var colors = [{ name: "yellow", color: "#fff281" }, { name: "red", color: "#ff4f4f" }, { name: "blue", color: "#a5d1ff" }, { name: "green", color: "#b0ffa5" }, { name: "black", color: "#2a2825" }, { name: "orange", color: "#ffb66f" }, { name: "pink", color: "#ffabd4" }, { name: "purple", color: "#dc7cff" }];
 
 var colorGenerator = function colorGenerator(colors) {
   while (gamelist.length < 30) {
@@ -13,3 +16,12 @@ var colorGenerator = function colorGenerator(colors) {
 colorGenerator(colors);
 console.log("lol");
 console.log(gamelist);
+
+// append li to gamelist
+// if first li element text equals to first item in array
+//remove that li, and remove the first item off the array
+//repeat until array.length = 0
+
+//on gameStart()
+//hide title div, unhide gamesession
+//create gamelist and display
--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ let gamelist = [];
 //need to make a key value object for each color
 //to choose the actual shade that I want...or maybe I can do that in scss...
 const colors = [
-  "yellow",
-  "red",
-  "blue",
-  "green",
-  "black",
-  "orange",
-  "pink",
-  "purple"
+  {name: "yellow", color: "#fff281"},
+  {name: "red", color: "#ff4f4f"},
+  {name: "blue", color: "#a5d1ff"},
+  {name: "green", color: "#b0ffa5"},
+  {name: "black", color: "#2a2825"},
+  {name: "orange", color: "#ffb66f"},
+  {name: "pink", color: "#ffabd4"},
+  {name: "purple", color: "#dc7cff"}
 ];
 
 const colorGenerator = (colors) => {
@@ -25,3 +25,21 @@ console.log("lol");
 console.log(gamelist);
 
 
+// append li to gamelist
+// if first li element text equals to first item in array
+//remove that li, and remove the first item off the array
+//repeat until array.length = 0
+
+//on gameStart()
+//hide title div, unhide gamesession
+//create gamelist and display
+
+const gameStart = () => {
+  colorGenerator(colors);
+  //hide title screen
+  //game list display
+}
+
+list = gamelist.reduce((result, color) => {
+  result += `<li style="color: ${color.color};">${color.name}</li>`
+});
